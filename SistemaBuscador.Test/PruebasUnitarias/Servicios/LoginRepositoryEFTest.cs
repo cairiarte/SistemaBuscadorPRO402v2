a7b46b0be00832e7ec564acdd3538ea0137e18c2
CARLOS AURELIO IRIARTE IRIARTE <CARLOS.IRIARTE@correoaiep.cl>
2021-09-23 20:07:47 -0300

    Cambios 28.08.2021

---
+++ SistemaBuscador.Test/PruebasUnitarias/Servicios/LoginRepositoryEFTest.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SistemaBuscador.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBuscador.Test.PruebasUnitarias.Servicios
+{
+    [TestClass]
+    public class LoginRepositoryEFTest : TestBase
+    {
+        [TestMethod]
+        public async Task UsuarioNoExiste() 
+        {
+            //Preparacion
+            var nombreBd = Guid.NewGuid().ToString();
+            var context = BuildContext(nombreBd);
+            context.Usuarios.Add(new Entities.Usuario() { NombreUsuario = "Usuario1", Password = "Password1" });
+            await context.SaveChangesAsync();
+
+            var context2 = BuildContext(nombreBd);
+
+            //Ejecucion
+            var nombreUsuario = "Usuario2";
+            var Password = "Password2";
+            var repo = new LoginRepositoryEF(context2);
+            var respuesta = await repo.UserExist(nombreUsuario, Password);
+
+            //Verificacion
+            Assert.IsFalse(respuesta);
+        }
+        [TestMethod]
+        public async Task UsuarioExiste() 
+        {
+            //Preparacion
+            var nombreBd = Guid.NewGuid().ToString();
+            var context = BuildContext(nombreBd);
+            context.Usuarios.Add(new Entities.Usuario() { NombreUsuario = "Usuario1", Password = "Password1" });
+            await context.SaveChangesAsync();
+
+            var context2 = BuildContext(nombreBd);
+
+            //Ejecucion
+            var nombreUsuario = "Usuario1";
+            var Password = "Password1";
+            var repo = new LoginRepositoryEF(context2);
+            var respuesta = await repo.UserExist(nombreUsuario, Password);
+
+            //Verificacion
+            Assert.IsTrue(respuesta);
+
+        }
+    }
+
+}
--- SistemaBuscador/Repositories/LoginRepository.cs
+++ SistemaBuscador/Repositories/LoginRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -6,9 +7,17 @@ using System.Threading.Tasks;
 
 namespace SistemaBuscador.Repositories
 {
-    public class LoginRepository
+    public class LoginRepository : ILoginRepository
     {
-        public bool UserExist(string usuario, string password) 
+        public void SetSessionAndCookie(HttpContext context)
+        {
+            Guid sessionId = Guid.NewGuid();
+            context.Session.SetString("sessionId", sessionId.ToString());
+            context.Response.Cookies.Append("sessionId", sessionId.ToString());
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> UserExist(string usuario, string password) 
         {
             bool result = false;
             string ConnectionString = "server=localhost;database=SistemaBuscador;Integrated Security=true;";
@@ -17,7 +26,7 @@ namespace SistemaBuscador.Repositories
             cmd.CommandType = System.Data.CommandType.StoredProcedure;
             cmd.Parameters.Add(new SqlParameter("@user", usuario));
             cmd.Parameters.Add(new SqlParameter("@password", password));
-            sql.Open();
+            await sql.OpenAsync();
             int bdResult = (int)cmd.ExecuteScalar();
             if (bdResult > 0) 
             {
